16f2603d196b63b3add44991d6ad7eb583cbf9bc
gkushniryov <blackmoth@live.ru>
2015-10-05 14:14:38 +0600

    local-backup: restore can replace files
    restore by relative path
    select replace/copy operation on restore

--- local-backup/restore.sh
+++ local-backup/restore.sh
@@ -12,47 +12,91 @@
 # sept 2014, GK
 #
 
-FNAME="$1"
+[ "$1" == "" ] && { echo "ERROR: missing param <file_name>"; exit 1; }
+[ -f "$1" ] || { echo "ERROR: file does not exists: '$1'"; exit 1; }
 
-if [ "$FNAME" == "" ]; then
-  echo "ERROR: missing param <file_name>"
-  exit 1
-fi
+FPATH="$(readlink -e $1)"
+FNAME="${FPATH##*/}"
+FDIR="${FPATH%/*}"
 
-echo "INFO: search for '$FNAME'"
 
 BAKDIR=~/bak
 
-FILES=()
+PTF="${BAKDIR}${FDIR}"
 #
 # example: /home/user/bak/home/user/.cse/local-backup/bak.sh:2014-09-18_12-31
-for LINE in `find $BAKDIR -name "$FNAME*"` ; do
-  # extract filename:date
-  T=`echo "$LINE" | sed -n "s|.*/\([^/]\+:.*\)$|\1|p"`
-  FILES=( ${FILES[*]} $T )
-done
+echo "INFO: search for '$FNAME' in '$PTF'"
 
-echo "INFO: ${FILES[*]} founded"
-
-SELFILE=''
-PS3='Select file version: '
-select OPT in ${FILES[*]} 'cancel'
+I=1
+INDEXES="$I"
+echo "[$I] cancel"
+while read ITEM # do not using sub-shell ( pass data to loop at end '<<< find ...' )
 do
+  I=$(( $I + 1 ))
+  INDEXES="$INDEXES $I"
+  echo "[$I] $ITEM"
+done <<< "$( find "$PTF" -name "$FNAME*" | sed -nr 's|.*/([^/]+):([^_]+)_(.*)$|\1   /   \2 \3|p' )"
 
-  if [ "$OPT" == 'cancel' ]; then
-    echo "INFO: cancel"
-    exit 0
-  fi
+read -p 'Select file version: ' ANS
 
-  SELFILE="$OPT"
-  break
+# check input
+case "$INDEXES" in
+  *$ANS*)
+    echo "your input: $ANS"
+    ;;
+  *)
+    echo "ERROR: wrong input: '$ANS', available: '$INDEXES'"
+    exit 3
+    ;;
+esac
 
-done
+# cancel
+if [ "$ANS" = "1" ]; then
+  echo 'canceled'
+  exit 0
+fi
+
+# get selected file absolute path
+SELFILE=$( find "$PTF" -name "$FNAME*" | sed -n "${ANS}p" )
 echo "you select: $SELFILE"
 
-FP=`find $BAKDIR -name "$SELFILE"`
+if ! [ -f "$SELFILE" ]; then
+  echo "ERROR: file not exists '$SELFILE'"
+  exit 2
+fi
 
-echo "DEBUG: FP=$FP"
+cp_print() {
+  if [ -f "$2" ]; then
+    echo "save $2 to $BAKDIR/last-replaced"
+    cp "$DEST_WT" "$BAKDIR/last-replaced" # bacup last replaced file
+  fi
+  echo "COPY $1 TO $2"
+  cp "$1" "$2"
+}
 
-cp $FP ./$SELFILE
+# process file
+PS3='Select file operation: '
+select OPT in cancel copy replace
+do
+  case $OPT in
+  cancel)
+    echo "canceled"
+    exit 0
+    ;;
+  copy)
+    DEST="${SELFILE#$BAKDIR}" # remove bak dir prefix from full path
+    cp_print "$SELFILE" "$DEST"
+    ;;
+  replace)
+    DEST="${SELFILE#$BAKDIR}" # remove bak dir prefix from full path
+    DEST_WT=${DEST%:*} # remove all after ':'
+    cp_print "$SELFILE" "$DEST_WT"
+    ;;
+  *)
+    echo "ERROR: unknown file operation '$OPT'"
+    exit 3
+    ;;
+  esac
+  break # select
+done
 
