27fa65087d73d71062912722e1d5814929736ed0
gkushniryov <blackmoth@live.ru>
2015-10-05 14:14:17 +0600

    gradle module: use build.gradle.template

--- gradle/module.sh
+++ gradle/module.sh
@@ -16,6 +16,8 @@ alias 'gradle-init-java'="gradle_init_java"
 
 # -- functions --
 
+__MOD_DIR="$MOD_DIR"
+
 # init gradle java project structure
 #
 gradle_init_java() {
@@ -34,41 +36,43 @@ gradle_init_java() {
     echo "build gradle file already exists: $BUILD_GRADLE_FILE"
     echo "overwrite? [yes/no]"
     read ANS
-    if [ "$ANS" = "yes" ]; then
+    if [ "$ANS" != "yes" ]; then
       p ERROR "do not override already exists build gradle file: $BUILD_GRADLE_FILE"
-      exit 1
+      return 1
     fi
     p INFO "overwrite already exists build gradle file: $BUILD_GRADLE_FILE"
   fi
 
-  echo '# gradle build script
+  local LOGGER_ENABLED=true
+  local GUAVA_ENABLED=true
 
-apply plugin: "java"
-
-ext {
+  # template variables
+  if [ "$LOGGER_ENABLED" = "true" ]; then
+    local LOGGER_VERSION='
   log4j2Version = "2.1"
-  slf4jVersion = "1.7.5"
-  guavaVersion = "18.0"
-}
-
-
-repositories {
-  mavenCentral()
-}
-
-dependencies {
-  // test
-  testCompile "junit:junit:4.4"
-
+  slf4jVersion  = "1.7.5"
+    '
+    local LOGGER_DEPENDENCIES='
   // logging
   compile "commons-logging:commons-logging:1.1.3"
   compile "org.slf4j:jul-to-slf4j:${slf4jVersion}"
   compile "org.slf4j:slf4j-api:${slf4jVersion}"
   compile "org.apache.logging.log4j:log4j-api:${log4j2Version}"
+    '
+  fi
 
-}
+  if [ "$GUAVA_ENABLED" = "true" ]; then
+    local GUAVA_VERSION='
+  guavaVersion = "18.0"
+    '
+    local GUAVA_DEPENDENCIES='
+  compile "com.google.guava:guava:${guavaVersion}"
+    '
+  fi
 
-  ' > $BUILD_GRADLE_FILE
+  # write build.gradle from template
+  local TEMPLATE=`cat "$__MOD_DIR/build.gradle.template"`
+  eval "echo \"${TEMPLATE}\"" > ${BUILD_GRADLE_FILE}
 
 }
 
